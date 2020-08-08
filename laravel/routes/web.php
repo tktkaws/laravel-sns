ed28f31c7ecb67e7f49cd16450847c6eb0e9e9a2
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-09 06:35:40 +0900

    fix routing for articles_CRUD

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,4 +12,5 @@
 */
 
 Auth::routes(); //-- この行を追加
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index'); //-- この行を編集
+Route::resource('/articles', 'ArticleController')->except(['index']); //-- この行を編集
