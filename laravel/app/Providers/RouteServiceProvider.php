17781d8e0e278f7f06f3cfd7ee085c7af033f344
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-08 14:57:17 +0900

    change route for redirect

--- laravel/app/Providers/RouteServiceProvider.php
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -21,7 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/'; // この行を変更('/home'を'/'にする)
+
 
     /**
      * Define your route model bindings, pattern filters, etc.
