f4e9303637102c99f2fdad7071d3b1afede100df
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-09 07:06:01 +0900

    create articles_create_form with fillable

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -10,6 +10,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Article extends Model
 {
     //==========ここから追加==========
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    //==========ここまで追加==========
+    //==========ここから追加==========
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 //==========ここから追加==========
 use App\Article;
 //==========ここまで追加==========
+//===========ここから追加==========
+use App\Http\Requests\ArticleRequest;
+//===========ここまで追加==========
 
 use Illuminate\Http\Request;
 
@@ -28,4 +31,14 @@ class ArticleController extends Controller
         return view('articles.create');
     }
     //==========ここまで追加==========
+
+     //==========ここから追加==========
+     public function store(ArticleRequest $request, Article $article)
+     {
+         $article->fill($request->all()); //-- この行を追加
+         $article->user_id = $request->user()->id;
+         $article->save();
+         return redirect()->route('articles.index');
+     }
+     //==========ここまで追加==========
 }
