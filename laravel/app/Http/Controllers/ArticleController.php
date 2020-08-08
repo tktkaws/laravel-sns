c09be2611fa10ea2f54bee0e137392908e670c99
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-08 14:29:30 +0900

    fix controller

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+//==========ここから追加==========
+use App\Article;
+//==========ここまで追加==========
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -10,39 +14,9 @@ class ArticleController extends Controller
     //==========ここから追加==========
     public function index()
     {
-        // ダミーデータ
-        $articles = [
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'ユーザー名1',
-                ],
-            ],
-            (object) [
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文2',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'ユーザー名2',
-                ],
-            ],
-            (object) [
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文3',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ユーザー名3',
-                ],
-            ],
-        ];
+         //==========ここから追加==========
+         $articles = Article::all()->sortByDesc('created_at');
+         //==========ここまで追加==========
 
         return view('articles.index', ['articles' => $articles]);
     }
