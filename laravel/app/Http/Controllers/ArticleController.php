6693a36e523a2b1b997aeb2fff01e41021a104c2
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-09 09:38:57 +0900

    create function of edit & destroy

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -47,4 +47,15 @@ class ArticleController extends Controller
         return view('articles.edit', ['article' => $article]);
     }
     //==========ここまで追加==========
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
 }
