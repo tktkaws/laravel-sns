bd9ad227aea922db5aa1e673c1a1b62f8c7d0d1a
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-09 13:48:07 +0900

    complete creating like function

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <button type="button" class="btn m-0 p-1 shadow-none">
-      <i class="fas fa-heart mr-1" :class="{'red-text':this.isLikedBy}" />
+      <i
+        class="fas fa-heart mr-1"
+        :class="{'red-text':this.isLikedBy, 'animated heartBeat fast':this.gotToLike}"
+        @click="clickLike"
+      />
     </button>
     {{ countLikes }}
-    <!-- この行を変更 -->
   </div>
 </template>
 
@@ -15,20 +18,52 @@ export default {
       type: Boolean,
       default: false
     },
-    //==========ここから追加==========
     initialCountLikes: {
       type: Number,
       default: 0
+    },
+    //==========ここから追加==========
+    authorized: {
+      type: Boolean,
+      default: false
+    },
+    endpoint: {
+      type: String
     }
     //==========ここまで追加==========
   },
   data() {
     return {
       isLikedBy: this.initialIsLikedBy,
-      //==========ここから追加==========
-      countLikes: this.initialCountLikes
-      //==========ここまで追加==========
+      countLikes: this.initialCountLikes,
+      gotToLike: false //==========この行を追加
     };
+  },
+  //==========ここから追加==========
+  methods: {
+    clickLike() {
+      if (!this.authorized) {
+        alert("いいね機能はログイン中のみ使用できます");
+        return;
+      }
+
+      this.isLikedBy ? this.unlike() : this.like();
+    },
+    async like() {
+      const response = await axios.put(this.endpoint);
+
+      this.isLikedBy = true;
+      this.countLikes = response.data.countLikes;
+      this.gotToLike = true; //==========この行を追加
+    },
+    async unlike() {
+      const response = await axios.delete(this.endpoint);
+
+      this.isLikedBy = false;
+      this.countLikes = response.data.countLikes;
+      this.gotToLike = false; //==========この行を追加
+    }
   }
+  //==========ここまで追加==========
 };
 </script>
