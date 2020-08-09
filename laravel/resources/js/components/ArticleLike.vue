2cba236c0ebecce80728ad6ea98d03ad0bbaf991
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-09 12:56:13 +0900

    manage data_dlow for vue_component

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
     <button type="button" class="btn m-0 p-1 shadow-none">
-      <i class="fas fa-heart mr-1" />
+      <i
+        class="fas fa-heart mr-1"
+        #----------ここから追加----------
+        :class="{'red-text':this.isLikedBy}"
+        #----------ここまで追加----------
+      />
     </button>
     10
   </div>
 </template>
 
 <script>
+//==========ここから追加==========
+export default {
+  props: {
+    initialIsLikedBy: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      isLikedBy: this.initialIsLikedBy
+    };
+  }
+};
+//==========ここまで追加==========
 </script>
